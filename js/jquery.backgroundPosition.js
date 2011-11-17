1e1ad4ffeccc7d223d0962369faedc388db71f8b
jhesus <jhesus@jhesus-laptop>
2011-11-16 21:11:22 -0430

    -. Arreglada la visualizacion de chekbox estilo iPhone

--- js/jquery.backgroundPosition.js
+++ js/jquery.backgroundPosition.js
@@ -1,13 +1,16 @@
 /**
  * @author Alexander Farkas
- * v. 1.1
+ * v. 1.22
  */
 
-(function($){
-	
-	if(!document.defaultView || !document.defaultView.getComputedStyle){
-		var oldCurCSS = jQuery.curCSS;
-		jQuery.curCSS = function(elem, name, force){
+
+(function($) {
+	if(!document.defaultView || !document.defaultView.getComputedStyle){ // IE6-IE8
+		var oldCurCSS = $.curCSS;
+		$.curCSS = function(elem, name, force){
+			if(name === 'background-position'){
+				name = 'backgroundPosition';
+			}
 			if(name !== 'backgroundPosition' || !elem.currentStyle || elem.currentStyle[ name ]){
 				return oldCurCSS.apply(this, arguments);
 			}
@@ -18,9 +21,18 @@
 			return oldCurCSS(elem, 'backgroundPositionX', force) +' '+ oldCurCSS(elem, 'backgroundPositionY', force);
 		};
 	}
-})(jQuery);
-
-(function($) {
+	
+	var oldAnim = $.fn.animate;
+	$.fn.animate = function(prop){
+		if('background-position' in prop){
+			prop.backgroundPosition = prop['background-position'];
+			delete prop['background-position'];
+		}
+		if('backgroundPosition' in prop){
+			prop.backgroundPosition = '('+ prop.backgroundPosition;
+		}
+		return oldAnim.apply(this, arguments);
+	};
 	
 	function toArray(strg){
 		strg = strg.replace(/left|top/g,'0px');
@@ -32,7 +44,6 @@
 	
 	$.fx.step. backgroundPosition = function(fx) {
 		if (!fx.bgPosReady) {
-			
 			var start = $.curCSS(fx.elem,'backgroundPosition');
 			if(!start){//FF2 no inline-style fallback
 				start = '0px 0px';
@@ -40,14 +51,13 @@
 			
 			start = toArray(start);
 			fx.start = [start[0],start[2]];
-			
-			var end = toArray(fx.options.curAnim.backgroundPosition);
+			var end = toArray(fx.end);
 			fx.end = [end[0],end[2]];
 			
 			fx.unit = [end[1],end[3]];
 			fx.bgPosReady = true;
 		}
-		
+		//return;
 		var nowPosX = [];
 		nowPosX[0] = ((fx.end[0] - fx.start[0]) * fx.pos) + fx.start[0] + fx.unit[0];
 		nowPosX[1] = ((fx.end[1] - fx.start[1]) * fx.pos) + fx.start[1] + fx.unit[1];           
